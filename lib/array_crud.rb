d822614dbeb7a5ca7736f8c453098c36e4fd3f01
Danniely Santos <dannielyingrid@gmail.com>
2020-08-05 04:25:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,23 +18,25 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  remove_element_from_end_of_array.pop
+  array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
-  remove_element_from_start_of_array.shift
+  array.shift
   
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array.index 
 end
 
 def retrieve_first_element_from_array(array)
+  array.first 
   
 end
 
 def retrieve_last_element_from_array(array)
+  array.last 
   
 end
